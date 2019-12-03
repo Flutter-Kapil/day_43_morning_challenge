eb006866c587409c7fe8fded7421c8b585336146
kapil jhajhria <jhajhria44@gmail.com>
2019-12-03 09:06:53 +0530

    complete

--- bin/main.dart
+++ bin/main.dart
@@ -19,5 +19,38 @@
 /// Input : 49927398716
 /// Output: 49927398716 passes the test
 
+bool verifyCard(int ccNumber) {
+  String reversedCcNumber =
+      ccNumber.toString().split('').reversed.join().toString();
+  print(reversedCcNumber); // #debug
+  int s1 = 0;
+  int s2 = 0;
+  //step 1 for s1
+  for (int i = 0; i < reversedCcNumber.length; i++) {
+    if (i % 2 == 0) {
+      s1 = s1 + int.parse(reversedCcNumber[i]);
+    }
+  }
+
+  print(s1); //#debug
+  //step 2 for s2
+  for (int i = 0; i < reversedCcNumber.length; i++) {
+    if (i % 2 != 0) {
+      int product = (int.parse(reversedCcNumber[i]) * 2);
+      if (product > 9) {
+        s2 = s2 + int.parse(product.toString()[1]) + 1;
+      } else {
+        s2 = s2 + product;
+      }
+    }
+  }
+  print(s2); // #debug
+  int totalSum = s1 + s2;
+
+  return totalSum.toString()[totalSum.toString().length - 1] == '0';
+}
+
 main() {
+  print(verifyCard(49927398716));
+  print(verifyCard(2581));
 }
--- test/Day_43_Morning_Challenge_test.dart
+++ test/Day_43_Morning_Challenge_test.dart
@@ -1,6 +1,9 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
   test('Test Case 1', () {
+    expect(verifyCard(49927398716), true);
+    expect(verifyCard(2581), false);
   });
 }
