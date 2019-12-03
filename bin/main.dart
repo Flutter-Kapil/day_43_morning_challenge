63a1186c4390d63df2708aaacdeb9fc46ae32d53
kapil jhajhria <jhajhria44@gmail.com>
2019-12-03 09:34:14 +0530

    refactor

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-///  Is this a valid credit card number?
+///  Is this a valid credit card selectedNumber?
 ///  Are credit card numbers just a random combination of the digits from 0-9? NO!
 //  Credit card numbers are a systematic combination of numbers that can satisfy
 //  a single test. This test is created so that errors can be avoided while
@@ -6,15 +6,15 @@
 //
 //  The algorithm is as follows:
 //
-//  - Reverse the order of the digits in the number.
+//  - Reverse the order of the digits in the selectedNumber.
 //  - Take the first, third, ... and every other odd digit in the reversed digits
 //  and sum them to form the partial sum s1
 //  - Taking the second, fourth ... and every other even digit in the reversed digits:
 //        - Multiply each digit by two and sum the digits if the answer is greater
 //          than nine to form partial sums for the even digits
 //        - Sum the partial sums of the even digits to form s2
-//  - If s1 + s2 ends in zero then the original number is in the form of a valid
-//  credit card number as verified by the Luhn test.
+//  - If s1 + s2 ends in zero then the original selectedNumber is in the form of a valid
+//  credit card selectedNumber as verified by the Luhn test.
 //  Example
 /// Input : 49927398716
 /// Output: 49927398716 passes the test
@@ -26,12 +26,13 @@ bool verifyCard(int ccNumber) {
   int s1 = 0;
   int s2 = 0;
   for (int i = 0; i < reversedCcNumber.length; i++) {
+    int selectedNumber = int.parse(reversedCcNumber[i]);
     if (i % 2 == 0) {
       //calculate s2
-      s1 = s1 + int.parse(reversedCcNumber[i]);
+      s1 = s1 + selectedNumber;
     } else {
       //calculate s2
-      int product = (int.parse(reversedCcNumber[i]) * 2);
+      int product = (selectedNumber * 2);
       if (product > 9) {
         s2 = s2 + int.parse(product.toString()[1]) + 1;
       } else {
